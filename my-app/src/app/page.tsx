71717b2ebff28609f6499e4847c2774992a6528f
Bryan Matthew <bryanmatthew206@gmail.com>
2024-02-07 22:41:03 +0800

    style: add app description in home page

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,76 @@
+import Menu from "@/components/Menu";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 gradient-background">
-            <div>test</div>
+        <main className="p-10 gradient-background relative">
+            <nav>
+                <div className="flex items-center gap-2 absolute">
+                    <img
+                        alt="favicon"
+                        loading="lazy"
+                        width="35"
+                        height="35"
+                        decoding="async"
+                        data-nimg="1"
+                        className="dark:invert"
+                        style={{ color: "transparent" }}
+                        src="logo.svg"
+                    />
+                    <h1 className="font-bold text-2xl">Cognimate</h1>
+                </div>
+                <div className="flex justify-center">
+                    <Menu />
+                </div>
+            </nav>
+            <div className="min-h-[90vh] flex flex-col items-center justify-center gap-5">
+                <h1 className="text-5xl font-bold">Cognimate 🤖.</h1>
+                <p className="text-2xl">
+                    Your all-in-one AI Study Assistant designed to elevate your
+                    learning experience
+                </p>
+                <Button>Get Started</Button>
+            </div>
+            <div className="flex h-screen items-center justify-around px-4">
+                <div className="flex w-3/12 flex-col gap-2">
+                    <h1 className="text-4xl font-bold">Elevate Learning, </h1>
+                    <h1 className="text-4xl font-bold">Elevate You.</h1>
+                    <p className="text-wrap text-xl">
+                        Unlock powerful learning experiences effortlessly with
+                        Cognimate‘s AI course builder.
+                    </p>
+                </div>
+                <img
+                    alt="feature-showcase"
+                    loading="lazy"
+                    width="2560"
+                    height="1600"
+                    decoding="async"
+                    data-nimg="1"
+                    className="w-5/12 border shadow-lg"
+                    style={{ color: "transparent" }}
+                />
+            </div>
+            <div className="flex h-screen items-center justify-around px-4">
+                <img
+                    alt="feature-showcase"
+                    loading="lazy"
+                    width="2560"
+                    height="1600"
+                    decoding="async"
+                    data-nimg="1"
+                    className="w-5/12 border shadow-lg"
+                    style={{ color: "transparent" }}
+                />
+                <div className="flex w-3/12 flex-col gap-2">
+                    <h1 className="text-4xl font-bold">
+                        Learning Has Never Been Easier.
+                    </h1>
+                    <p className="text-wrap text-xl">
+                        Personalize your learning based on your focus today.
+                    </p>
+                </div>
+            </div>
         </main>
     );
 }
---
+++ my-app/src/components/Menu.tsx
@@ -0,0 +1,59 @@
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarTrigger,
+} from "@/components/ui/menubar";
+import { FunctionComponent } from "react";
+
+interface Props {}
+
+const Menu: FunctionComponent<Props> = () => {
+    return (
+        <Menubar>
+            <MenubarMenu>
+                <MenubarTrigger>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        className="lucide lucide-sun h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+                    >
+                        <circle cx="12" cy="12" r="4"></circle>
+                        <path d="M12 2v2"></path>
+                        <path d="M12 20v2"></path>
+                        <path d="m4.93 4.93 1.41 1.41"></path>
+                        <path d="m17.66 17.66 1.41 1.41"></path>
+                        <path d="M2 12h2"></path>
+                        <path d="M20 12h2"></path>
+                        <path d="m6.34 17.66-1.41 1.41"></path>
+                        <path d="m19.07 4.93-1.41 1.41"></path>
+                    </svg>
+                </MenubarTrigger>
+                <MenubarContent>
+                    <MenubarItem>Light</MenubarItem>
+                    <MenubarItem>Dark</MenubarItem>
+                    <MenubarItem>System</MenubarItem>
+                </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>Home 🏠</MenubarTrigger>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>Course Builder 🔨</MenubarTrigger>
+            </MenubarMenu>
+            <MenubarMenu>
+                <MenubarTrigger>AI Summarize ✨</MenubarTrigger>
+            </MenubarMenu>
+        </Menubar>
+    );
+};
+
+export default Menu;
