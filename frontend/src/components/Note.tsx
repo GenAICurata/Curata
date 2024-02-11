21f2e2d486c91c94889fe2a5c3f30945e4d1fb7e
Bryan Matthew <bryanmatthew206@gmail.com>
2024-02-12 03:06:36 +0800

    feat: finish course builder

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,10 +1,8 @@
 "use client";
 import {
     Drawer,
-    DrawerClose,
     DrawerContent,
     DrawerDescription,
-    DrawerFooter,
     DrawerHeader,
     DrawerTitle,
     DrawerTrigger,
@@ -13,6 +11,7 @@ import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import { Button } from "./ui/button";
 import "react-quill/dist/quill.snow.css";
+import { motion } from "framer-motion";
 
 import { FunctionComponent } from "react";
 
@@ -23,29 +22,41 @@ const Note: FunctionComponent<Props> = () => {
 
     return (
         <Drawer>
-            <DrawerTrigger className="fixed bottom-5 right-5 p-4">
-                <div className="bg-white rounded-full p-8 border-2">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="lucide lucide-notebook-pen "
-                    >
-                        <path d="M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4"></path>
-                        <path d="M2 6h4"></path>
-                        <path d="M2 10h4"></path>
-                        <path d="M2 14h4"></path>
-                        <path d="M2 18h4"></path>
-                        <path d="M18.4 2.6a2.17 2.17 0 0 1 3 3L16 11l-4 1 1-4Z"></path>
-                    </svg>
-                </div>
-            </DrawerTrigger>
+            <motion.div
+                className="fixed bottom-5 right-5 p-4"
+                animate={{
+                    opacity: [0.3, 1, 0.3], // Opacity values to animate between
+                }}
+                transition={{
+                    duration: 3,
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                }}
+            >
+                <DrawerTrigger className="fixed bottom-5 right-5 p-4">
+                    <div className="bg-white rounded-full p-8 border-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="lucide lucide-notebook-pen "
+                        >
+                            <path d="M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4"></path>
+                            <path d="M2 6h4"></path>
+                            <path d="M2 10h4"></path>
+                            <path d="M2 14h4"></path>
+                            <path d="M2 18h4"></path>
+                            <path d="M18.4 2.6a2.17 2.17 0 0 1 3 3L16 11l-4 1 1-4Z"></path>
+                        </svg>
+                    </div>
+                </DrawerTrigger>
+            </motion.div>
             <DrawerContent>
                 <div className="flex justify-center">
                     <DrawerHeader>
